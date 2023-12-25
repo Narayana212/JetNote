7a0961af514f0a6b4d2b7609102aec9d4b11d70d
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-12-25 23:53:39 +0530

    add gariny background

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className=`${inter.className} grainy`>
         <ConvexClientProvider>
           <EdgeStoreProvider>
             <ThemeProvider
